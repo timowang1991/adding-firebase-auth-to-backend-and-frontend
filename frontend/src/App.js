bff9d09828901862f0a3d67a841d88dd074c3b9c
Timo Wang <timowang1991@gmail.com>
2021-11-27 22:28:34 +0800

    firebase FE ready

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import { getAuth, signInWithPopup, onAuthStateChanged, getIdToken, GoogleAuthProvider } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import ListOfTodo from './components/ListOfTodo';
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+function App() {
+
+  const [gAuth, setGAuth] = useState(false || !!window.localStorage.getItem('auth'));
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (result) => {
+      console.log('onAuthStateChanged--', result);
+      if (result) {
+        setGAuth(true);
+        window.localStorage.setItem('auth', true);
+        getIdToken(result).then((idToken) => {
+          console.log('idToken--', idToken);
+          setToken(idToken);
+        })
+      }
+    });
+  }, []);
+
+  const loginWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      console.log('signInWithPopup--', result);
+      if (result) {
+        setGAuth(true);
+        window.localStorage.setItem('auth', true);
+      }
+    }).catch((error) => {
+      console.log('-- error', error);
+    })
+  }
+
+  return (
+    <div className="App">
+      { gAuth ?
+        <ListOfTodo token={token} />
+        :
+        <button onClick={loginWithGoogle}>Login with Google</button>
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ListOfTodo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ListOfTodo ({ token }) {
+    return (
+        <div>
+            <h1>List of todo</h1>
+        </div>
+    );
+}
